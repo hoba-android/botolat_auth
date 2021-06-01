633e3e5f494c57c63d2ce6528a5ef682e69157fd
hoba-android <heba96@yahoo.com>
2021-06-01 14:36:48 +0200

    some validation

---
+++ src/Dummy.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+const Dummy = (props) => {
+  const logOut = () => {
+    props.navigation.navigate("Login");
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 20, fontFamily: "CairoRegular" }}>
+        أهلا وسهلا
+      </Text>
+      <TouchableOpacity style={styles.googleButton} onPress={logOut}>
+        <Text
+          style={{ fontSize: 20, fontFamily: "CairoRegular", color: "white" }}
+        >
+          تسجيل خروج
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Dummy;
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  googleButton: {
+    width: "80%",
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "space-around",
+
+    borderRadius: 25,
+    backgroundColor: "#d44638",
+    padding: 7,
+    marginVertical: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
